afddd3418fd7a36ec04382c930fa9f767d18b540
MasterPie <vrp@.(none)>
2011-02-19 03:53:00 -0500

    Set validations on everything

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,13 @@
 class Event < ActiveRecord::Base
+	belongs_to :user
+	has_and_belongs_to_many :affiliations
+	
+	validates_presence_of :name, :desc, :location, :time, :user_id, :categories
+	validates_size_of :name, :user_id :maximum => 50
+	validates_size_of :description, :image,  :maximum => 255
+	validates_size_of :location, :maximum => 100
+	validates_size_of :categories, :maximum => 2
+	validates_format_of :name, :desc, :location, :with=> /^[a-zA-Z0-9]* $/
+	
+	
 end
