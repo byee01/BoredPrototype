203abdf8bafb6baab75e3adbbe553d20931c1fce
MasterPie <vrp@.(none)>
2011-02-25 10:10:35 -0500

    Matched frontend with test scroll UI

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,13 +1,58 @@
 /*---------------------------
-Modal Dialogues
+ Bogus data generation
 ----------------------------*/
-$(document).ready(function() {
-     $('#new_event_btn').click(function(e) {
-          e.preventDefault();
-	  $('#create_event').reveal();
-     });
-});
 
+var numEvents = 50;
+var possibleEvents = [
+	{url:'axo.jpg', title:'Alpha Chi Omega Vera Bradley Sale', date:'Wednesday, Feb. 9'},
+	{url:'cit.jpg', title:'CIT Ball', date:'Friday, Feb. 18'},
+	{url:'disogni.jpg', title:'Disogni - Opening Reception', date:'Sunday, Mar. 5'},
+	{url:'help.jpg', title:'"Help Yourself" Resource Fair', date:'Monday, Feb. 14'},
+	{url:'isu.jpg', title:'ISU Valentine Rose Sale', date:'Saturday, Jan. 29'},
+	{url:'jsa.jpg', title:'JSA Bubble Pi Brioche Sale', date:'Thursday, Feb. 3'},
+	{url:'kesem.jpg', title:'Camp Kesem Info Session', date:'Tuesday, Feb. 16'},
+	{url:'korea.jpg', title:'Korean New Year: FREE DDOK GUK EVENT', date:'Thursday, Feb. 3'},
+	{url:'lg.jpg', title:'Lunar Gala: Melange', date:'Saturday, Feb. 5'},
+	{url:'onib.jpg', title:'One Night in Beijing Performance Auditions', date:'Wednesday, Feb. 9'},
+	{url:'romance.jpg', title:'Romance Kits: Valentine\'s Day Sale', date:'Monday, Feb. 14'},
+	{url:'rose.jpg', title:'Chocolate Rose Grams', date:'Monday, Feb. 14'},
+	{url:'sc2.jpg', title:'Conaboy VS Sit $200 SC2 Showmatch', date:'Thursday, Feb. 3'},
+	{url:'superbowl.jpg', title:'AB Special Events: Superbowl Screening', date:'Sunday, Feb. 6'},
+	{url:'titus.jpg', title:'Titus Adronicus with Passengers', date:'Sunday, Feb. 13'},
+	{url:'polo.jpg', title:'Women\'s Water Polo Bake Sale', date:'Monday, Feb. 28'},
+	{url:'famine.jpg', title:'CMU 30 Hour Famine', date:'Friday, Feb. 25'},
+	{url:'engie.jpg', title:'Mr. Engineer 2011', date:'Friday, Feb. 25'},
+	{url:'battle.jpg', title:'BATTLEZONE 4 Breakdance Battles @ CMU', date:'Saturday, Feb. 26'},
+	{url:'playoffs.jpg', title:'Playoffs', date:'Friday, Feb. 25'},
+	{url:'apple.jpg', title: 'Magical, Revolutionary, Unbelievably Priced Pt. 2', date:'Wednesday, Mar. 2'}
+	];
+
+$.shuffle(possibleEvents); // More randomness
+/*
+for(i=1; i<=numEvents; i++) {
+	var category = Math.ceil(Math.random()*10); // Pick category between one and three
+
+	if(i%(Math.ceil(Math.random()*numEvents)) == 0) { $.shuffle(possibleEvents) } // Shuffle randomly.
+
+	var eventData = possibleEvents[i%possibleEvents.length];
+	var eventBox = $("<div />", { id: "event-" + i, "class": "box col" + category});
+	var b = $("<img />", { src: "img/events/" + eventData.url, alt: eventData.title});
+	var c = $("<h3 />", { text: eventData.title });
+	var d = $("<p />", { text: eventData.date });
+
+	eventBox.append(b, c, d);
+
+	var eventDesc = $("<div />", {id: "event-" + i + "-desc", "class": "reveal-modal event-desc"});
+	var e = $("<h4 />", { text: eventData.title});
+	eventDesc.append(e);
+
+	for(var j = 0; j<category+3; j++) {
+		eventDesc.append($('<p>This is an event description.  Useful information goes here!</p>'));
+	}*/
+/*
+	//a.appendTo('#events');
+	$('#events').append(eventBox, eventDesc);
+}*/
 
 /*---------------------------
  Scroll function
@@ -62,33 +107,17 @@ $('#events').imagesLoaded( function(){
 	});
 	// This is an on-click hander for all the links in the menu bar.
 	$('#filtering-nav a').click(function(){
-	  var colorClass = '.' + $(this).attr('class');
+		var colorClass = '.' + $(this).attr('class');
 
-
-	  if(colorClass=='.all') {
-		// show all hidden boxes
-		$wall.children('.invis').toggleClass('invis').stop().fadeIn(speed);
-	  } else {  
-		// hide visible boxes 
-		$wall.children().not(colorClass).not('.invis').toggleClass('invis').stop().fadeOut(speed);
-		// show hidden boxes
-		$wall.children(colorClass+'.invis').toggleClass('invis').stop().fadeIn(speed);
-	  }
-	  $wall.masonry();
-
-	  return false;
+		if(colorClass=='.all') {
+			// show all hidden boxes
+			$wall.children('.invis').toggleClass('invis').stop().fadeIn(speed);
+			} else {  
+			// hide visible boxes 
+			$wall.children().not(colorClass).not('.invis').toggleClass('invis').stop().fadeOut(speed);
+			// show hidden boxes
+			$wall.children(colorClass+'.invis').toggleClass('invis').stop().fadeIn(speed);
+		}
+		$wall.masonry();
 	});
-	
-		$("#new_event").validate({
-		rules: {
-			"event[name]": {required:true},
-			"event[description]": {required:true},
-			"event[location]": {required:true},
-			"event[categories][]":{required: true, maxlength: 2}
-			}
-		});
-		
-		//$("#event_time").val("lol");
-		//$("#event_time").datetimepicker();
-	
 });
---
+++ public/javascripts/ui.js
@@ -0,0 +1,37 @@
+/*		BOREDCAST UI FUNCTIONS
+		Harold Kim
+		Last Updated: February 25th, 2011
+*/
+
+$(function() {
+	
+	/* CLEARS SEARCH ON CLICK */
+	$('#topSearch').focus(function(e) {
+		this.value = '';
+	});
+	
+	/* FAUX SEARCH FUNCTIONALITY */
+	$('#topSearch').keyup(function(e) {
+		$('.box:nth-child(5n)').fadeOut();
+		$('.box:nth-child(5n)').remove();
+	});
+	
+	/* CHANGES ACTIVE STATE */
+	$('.nav a').click(function(e) {
+		$('.select').removeClass('select');
+		console.log('herro world');
+		$(this).parent('li').addClass('select');
+	});
+	
+	/* CLEARS ALL TEXT FIELDS IN "ADD NEW EVENT" AS CLICKED */
+	$('#create_event input[type="text"]').focus(function(e) {
+		this.value = '';
+	});
+	
+	/* CHANGES POSTER AS STUFF IS TYPED */
+	$('#e-make-title').keyup(function(e){
+		var text = $(this).val();
+		$('#m-flyer-overlay').html('<h2>' + text + '</h2>');
+	});
+	
+});
