7281499f5a6cd50369840d112fc7a82aa034f16e
MasterPie <vrp@.(none)>
2011-02-28 01:53:58 -0500

    Cleaned up InfoDisplay project

--- InfoDisplay/MainWindow.xaml.cs
+++ InfoDisplay/MainWindow.xaml.cs
@@ -11,14 +11,9 @@ namespace KinectSpaceToWindowCoords
 {
     public partial class MainWindow : Window
     {
-        //private WebKitBrowserWrapper wkBrowser;
         private WebKitBrowser wkBrowser;
-        
-        static readonly Color HoverColor = Colors.Yellow;
-        Color oldColor;
 
         private System.Windows.Forms.Timer setupTimer;
-        //private System.Windows.Forms.Timer cursorTimer;
 
         public MainWindow()
         {
@@ -26,21 +21,11 @@ namespace KinectSpaceToWindowCoords
             this.DataContext = new MainWindowViewModel();
             App.Current.Exit += new ExitEventHandler(Current_Exit);
             this.KeyDown += new KeyEventHandler(MainWindow_KeyDown);
-            
-           // Mouse.OverrideCursor = mainCursor;
+           
             player.LoadedBehavior = player2.LoadedBehavior = player3.LoadedBehavior = System.Windows.Controls.MediaState.Manual;
             setupTimer = new System.Windows.Forms.Timer();
             setupTimer.Interval = 3000;
             setupTimer.Tick += new System.EventHandler(setupTimer_Tick);
-
-          /*  cursorTimer = new System.Windows.Forms.Timer();
-            cursorTimer.Interval = 1;
-            cursorTimer.Tick += new System.EventHandler(cursorTimer_Tick);*/
-        }
-
-        void cursorTimer_Tick(object sender, System.EventArgs e)
-        {
-            //Mouse.OverrideCursor = mainCursor;
         }
 
         void setupTimer_Tick(object sender, System.EventArgs e)
@@ -52,19 +37,17 @@ namespace KinectSpaceToWindowCoords
         public void PostInitialize()
         {
             MakeJSCall("$('body, container').css('overflow','hidden');");
-           // cursorTimer.Start();
         }
 
         public void MakeJSCall(string js)
         {
             try
             {
-                //wkBrowser.Browser.StringByEvaluatingJavaScriptFromString(js);
                 wkBrowser.StringByEvaluatingJavaScriptFromString(js);
             }
             catch (System.Exception h)
             {
-                InitializeComponent();
+                InitializeComponent(); // Restart UI
             }
         }
 
@@ -93,41 +76,14 @@ namespace KinectSpaceToWindowCoords
             CameraSession.ShutDown = true;
         }
 
-        void EllipseMouseEnter(object sender, MouseEventArgs e)
-        {
-            var ellipse = (Ellipse)sender;
-            oldColor = ((SolidColorBrush)ellipse.Fill).Color;
-            ellipse.Fill = new SolidColorBrush(HoverColor);
-        }
-
-        void EllipseMouseLeave(object sender, MouseEventArgs e)
-        {
-            var ellipse = (Ellipse)sender;
-            ellipse.Fill = new SolidColorBrush(oldColor);
-        }
+       
 
         private void browserContainer_Loaded(object sender, RoutedEventArgs e)
         {
             WindowsFormsHost host = new WindowsFormsHost();
-            //wkBrowser = new WebKitBrowserWrapper();
-            //host.Child = wkBrowser.Browser;
-            
-            //this.browserContainer.Children.Add(host);
-
-            //wkBrowser.Browser.Navigate("http://www.tinyurl.com/BoredCast1");
-            
-            //this.ViewModel.PropertyChanged += (o, f) =>
-            //{
-            //    if (f.PropertyName.Equals("URI"))
-            //    {
-            //        MakeJSCall(this.ViewModel.URI);                    
-            //        playSoundFeedback();
-            //    }
-            //};
-
+        
             wkBrowser = new WebKit.WebKitBrowser();
             host.Child = wkBrowser;
-            
 
             this.browserContainer.Children.Add(host);
 
