a70de8883ee7d23d473559059302d4dc54523911
MasterPie <vrp@.(none)>
2011-04-06 00:04:19 -0400

    Added sessions and added some dummy code in preparation with interaction with stafford

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+	def create
+		#render :text => request.env['rack.auth'].inspect
+		#@andrewID = 
+		#unless @auth = User.find_by_andrew_id(@andrew_id)
+		#	@auth = User.create
+		#	@auth = 
+		#end
+		#self.current_user = @auth.user
+		
+		#render :text => "Welcome, #{current_user.name}."
+	end
+end
