0ea364ca0f0b2239104fb6b834e31ca0ae9e935c
Brian Yee <brian.a.yee@gmail.com>
2011-09-26 01:34:16 -0400

    Missed that

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -11,6 +11,7 @@ $container.isotope({
 
 // Expand event when clicked
 $container.delegate('.event', 'click', function() {
+
   $(this).toggleClass('expanded');
   
   // This causes severe performance issues.
