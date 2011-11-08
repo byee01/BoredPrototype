4f3cf63edea0d87ce707753f0a91b75b24539cdd
Brian Yee <brian.a.yee@gmail.com>
2011-11-08 15:48:40 -0500

    Added paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,20 @@
 ActiveRecord::Schema.define(:version => 20110911205737) do
 
   create_table "events", :force => true do |t|
-    t.string   "name",        :null => false
-    t.text     "description", :null => false
-    t.string   "location",    :null => false
-    t.datetime "start_time",  :null => false
-    t.datetime "end_time",    :null => false
+    t.string   "name",                                :null => false
+    t.text     "description",                         :null => false
+    t.string   "location",                            :null => false
+    t.datetime "start_time",                          :null => false
+    t.datetime "end_time",                            :null => false
     t.string   "flyer"
     t.integer  "pattern"
     t.string   "categories"
+    t.integer  "approval_rating",    :default => 100
+    t.string   "approver_id"
+    t.string   "flyer_file_name"
+    t.string   "flyer_content_type"
+    t.integer  "flyer_file_size"
+    t.datetime "flyer_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
