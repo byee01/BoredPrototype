bec46b8f987ca097b6c8f72ba353ed577d7d0012
Brian Yee <brian.a.yee@gmail.com>
2011-11-08 21:53:09 -0500

    Removed backbone.js

--- app/assets/javascripts/collections/events.js
+++
@@ -1,8 +0,0 @@
-// Event collection
-
-//= require backbone
-
-var EventStore = Backbone.Collection.extend({
-  model: Event,
-  url: '/events'  
-});
--- app/assets/javascripts/models/event.js
+++
@@ -1,9 +0,0 @@
-// Events model
-
-var Event = Backbone.Model.extend({
-  url : function() {
-    var base = 'events';
-    if (this.isNew()) return base;
-    return base + (base.charAt(base.length - 1) == '/' ? '' : '/') + this.id;
-  }
-});
