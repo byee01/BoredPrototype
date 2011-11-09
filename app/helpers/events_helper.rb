f20b9520d949b838feab7e0d473071a499476555
Brian Yee <brian.a.yee@gmail.com>
2011-11-09 04:24:52 -0500

    Event positioning for categories

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -6,4 +6,27 @@ module EventsHelper
     # to compare nicely.
     Time.at((Time.now.advance(:minutes => minute_offset*30).to_f / (30*60) ).round * 30*60).strftime("%l:%M %P").lstrip
   end
+  @@category_hash = {
+  "Arts" => 1, 
+  "Sports" =>2,
+  "Professional" => 4,
+  "Cultural" => 6,
+  "Music" => 7,
+  "Movies" => 8,
+  "Academic" => 9,
+  "Social" => 10,
+  "Service" => 11
+  }
+
+  def self.all_categories
+    categories = []
+    @@category_hash.each do |c|
+      categories << c
+    end
+    return categories
+  end
+
+  def get_check_id(name)
+    'check-' + name.downcase
+  end
 end
